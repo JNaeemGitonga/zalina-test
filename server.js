99e1d280c3d0dad73538d73fe9a2c87a1f6e04e1
PROB8 <blackb8r@gmail.com>
2017-11-09 12:44:13 -0500

    change app.use and app.get

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const app  = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static('public'))
-console.log('shit', __dirname)
-app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    // console.log('this is dirname', __dirname)
-    const index = path.resolve(__dirname, './build', 'index.html');
+// app.use(express.static('public'))
+// console.log('shit', __dirname)
+// app.get(/^(?!\/api(\/|$))/, (req, res) => {
+//     // console.log('this is dirname', __dirname)
+//     const index = path.resolve(__dirname, './build', 'index.html');
     
-    res.sendFile(index);
-  });
+//     res.sendFile(index);
+//   });
  
+  app.use(express.static('public'))
+  
+  app.get('*', (req, res) => {
+      const index = path.resolve(__dirname, 'build', 'index.html');
+      res.sendFile(index);
+  });
+
 app.listen(port,  () => {
     console.log("Server's starting!!");
 }) 
