3425e4fa0901b6a072e7e262af018ddeb9013d66
PROB8 <blackb8r@gmail.com>
2017-11-08 23:21:09 -0500

    update app.use

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app  = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static('public'))
 
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
     const index = path.resolve(__dirname, '../client/build', 'index.html');
