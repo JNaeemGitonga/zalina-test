d2d24fbca06c10c2a6354175480b18b63e2b3313
PROB8 <blackb8r@gmail.com>
2017-11-09 11:59:22 -0500

    console.log(__dirname)

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app  = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
-
+console.log('shit')
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../build', 'index.html');
+    console.log(__dirname)
+    const index = path.resolve(__dirname, './build', 'index.html');
+    
     res.sendFile(index);
   });
-
+ 
 app.listen(port,  () => {
     console.log("Server's starting!!");
-})
+}) 
